b98c0333bcd7ee3a5055800245860fffbccd8467
Anderson17RJ <nobrega.inv95@gmail.com>
2025-11-07 13:08:07 -0300

    scraper.py2

--- scraper.py
+++ scraper.py
@@ -4,21 +4,33 @@ from concurrent.futures import ThreadPoolExecutor
 import logging
 
 BASE_URL = "https://www.thecrims.com"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/142.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Referer": "https://www.thecrims.com/"
+}
 logging.basicConfig(level=logging.DEBUG)
 
 def get_top50():
-    scraper = cloudscraper.create_scraper()
+    scraper = cloudscraper.create_scraper(
+        browser={"custom": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/142.0.0.0 Safari/537.36"}
+    )
+
+    
+
     url = f"{BASE_URL}/api/v1/stats/killers?country=&character=&level="
-    r = scraper.get(url, timeout=10)
+    r = scraper.get(url,headers=HEADERS, timeout=15)
     data = r.json()
     print(r.status_code)
     print(r.text[:500])
     return data["killers"][:50]
 
 def fetch_user(user_id):
-    scraper = cloudscraper.create_scraper()
+    scraper = cloudscraper.create_scraper(
+        browser={"custom": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/142.0.0.0 Safari/537.36"}
+    )
     url = f"{BASE_URL}/api/v1/user/{user_id}/stats"
-    r = scraper.get(url)
+    r = scraper.get(url,headers=HEADERS, timeout=15)
     data = r.json()
 
     stats_user = data["stats_user"]
